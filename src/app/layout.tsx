a6a293a183c77f71771a0d1ecf3d0b4d22f86a7f
ohsssuk <ohsssuk@naver.com>
2024-06-15 10:55:55 +0900

    v1 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import "@/styles/globals.css";
 import "@/styles/reset.css";
 
 export const metadata: Metadata = {
-  title: "Riiid",
-  description:
-    "Bring cutting-edge AI for democratization of quality education; Explore our AI solutions to get rid of “inefficiency,” “inconsistency,” and “inequality” in education.",
+  title: "TEST",
+  description: "",
 };
 
 export default function RootLayout({
