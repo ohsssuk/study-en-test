bf555d4946343f1afeeee2215a1ea86444d00bac
ohsssuk <ohsssuk@naver.com>
2024-06-15 15:08:05 +0900

    배포 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  // basePath 명시
+  basePath: isProduction ? "/study-en-test" : "",
+  // next 이미지 최적화 옵션 off
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
